b807f568fb98c24f4c0a3a124747f3a7a803a751
Steven Gangano <stevengangano@Stevens-MacBook-Pro.local>
2017-09-11 20:55:46 -0700

    updated

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get("/contact", function(req, res) {
 });
 
 //Runs on PORT localhost:5000
-var PORT = process.env.PORT || 5000
+var PORT = process.env.PORT || 7000
 
 app.listen(PORT, function(){
   console.log('Server Running');
